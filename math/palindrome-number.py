a68ac97b2691da80bcb23246ed897cdd0c44e38b
SARANGSSP <sarangsushantpandit23@gmail.com>
2024-09-25 12:40:38 +0530

    Solve palindrome-number (2024-09-25)

---
+++ math/palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        string= str(x)
+        rev_string = string[::-1]
+        if rev_string == string:
+            return True
+        else:
+            return False
+        
+        
