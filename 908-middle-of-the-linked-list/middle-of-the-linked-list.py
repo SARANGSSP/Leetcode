3263aefff0b6cb3cc592ded41c61b737b3d5fec1
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-07-06 00:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (57.18%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,23 +5,10 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = head
-        count = 0
-
-        while(curr):
-            count += 1
-            curr = curr.next
-
-        iterator = head
-        counter = 0
-        while(iterator):
-            temp = iterator.next
-            if counter == (count//2):
-                head = iterator
-                break
-            else:
-                iterator = temp
-                counter += 1
-        return head
-
-                
+        #second method is to create  afast and slow pointer
+        fast = head
+        slow = head
+        while fast and fast.next and slow:
+            fast = fast.next.next
+            slow = slow.next
+        return slow     
