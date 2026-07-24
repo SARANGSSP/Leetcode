19479c3ce59e6e0e0ff55f89f9c1f9654dba3503
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-07-25 02:21:22 +0530

    Time: 11 ms (76.17%) | Memory: 19.2 MB (92.48%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,15 +1,20 @@
-#the brute force is probabaly to go through every string and then check for max, if length is greater than max then we update max 
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        hashmap = {}
-        left = 0
-        maxi = 0
-        for right, char in enumerate(s):
-            if char in hashmap and hashmap[char] >= left:
-                left = hashmap[char] + 1
-            hashmap[char] = right
-            maxi = max(maxi, right - left + 1)
-        return maxi
+        n = len(s)
+        if len(s) == 1:
+            return 1
+        string = ""
+        maxlen = 0
+        for i in range(n):
+            if s[i] not in string:
+                string = string + s[i]
+                maxlen = max(len(string),maxlen)
+            else:
+                index = string.find(s[i])
+                string = string[index+1:]
+                string += s[i]
+                maxlen = max(len(string),maxlen)
+        maxlen = max(len(string),maxlen)
+        return maxlen
 
-        
+            
