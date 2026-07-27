253b6610a055d7f6122114c7a03229e5af5474b3
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-07-27 11:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.04%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -1,22 +1,13 @@
 #there are two ways of solving this: 1. is to sort and return max 2. is to find the two largest abs values and return their multiplication but only do that if both values are negative
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        neg_max, neg_sec, pos_max,pos_sec = 0,0,0,0
-        flag = False
+        pos_max,pos_sec = 0,0
         for i in range(len(nums)):
-            if nums[i] < 0 and nums[i] <= neg_max:
-                neg_sec = neg_max
-                neg_max = nums[i]
-                flag = True
-            elif nums[i] > 0 and nums[i] >= pos_max:
+            if nums[i] >= pos_max:
                 pos_sec = pos_max
                 pos_max = nums[i]
             else:
                 if nums[i] >= pos_sec:
                     pos_sec = nums[i]
         pos_prod = (pos_max - 1) * (pos_sec - 1)
-        neg_prod = (neg_max - 1) * (neg_sec -1)
-        if flag:
-            return max(pos_prod,neg_prod)
-        else:
-            return pos_prod
+        return pos_prod
