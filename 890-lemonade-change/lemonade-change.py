44fd73a848bb3944562e6c4f41b206de32ddf4c4
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-07-29 03:00:13 +0530

    Time: 3 ms (73.41%) | Memory: 23.4 MB (75.27%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,30 +1,21 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        n = len(bills)
-        count5,count10,count20,totalcount = 0,0,0,0
-        for i in range(n):
-            if bills[i] == 5:
-                count5 += 1
-                totalcount += 1
-                print(f"add5")
-            elif bills[i] == 10:
-                count10 += 1
-                print(f"add10")
-                if count5 > 0:
-                    totalcount += 1
-                    count5 -= 1
-            else:
-                count20 += 1
-                print(f"add20")
-                if count5 > 0 and count10 > 0:
-                    count10 -= 1
-                    count5 -= 1
-                    totalcount += 1
-                elif count5 >= 3:
-                    count5 -= 3
-                    totalcount += 1
-        if totalcount == n:
-            return True
-        else:
-            print(totalcount)
-            return False
+        five = 0
+        ten = 0
+        for b in bills:
+            if b == 5:
+                five += 1
+            elif b == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            elif b == 20:
+                if ten >= 1 and five >= 1:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
