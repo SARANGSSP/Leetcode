b5cf2934ba09ce27f73ae5d911e4e2d131b4f67e
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-01 15:16:54 +0530

    Time: 3042 ms (5.03%) | Memory: 19.2 MB (79.49%) - GetLeet

--- Medium/Array/Predict-the-Winner/Solution.py
+++ Medium/Array/Predict-the-Winner/Solution.py
@@ -3,9 +3,7 @@ class Solution:
         def best_diff(i, j):
             if i > j:
                 return 0
-            # option 1: take the left end
             take_left = nums[i] - best_diff(i + 1, j)
-            # option 2: take the right end
             take_right = nums[j] - best_diff(i, j - 1)
             return max(take_left, take_right)
 
