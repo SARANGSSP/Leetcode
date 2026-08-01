6355f3e631735eb8d39b6daa5d2ccb340bbe50a4
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-01 15:18:47 +0530

    Time: 39 ms (25.18%) | Memory: 19.2 MB (94.32%) - GetLeet

--- Medium/Array/Predict-the-Winner/Solution.py
+++ Medium/Array/Predict-the-Winner/Solution.py
@@ -1,10 +1,17 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def best_diff(i, j):
-            if i > j:
-                return 0
-            take_left = nums[i] - best_diff(i + 1, j)
-            take_right = nums[j] - best_diff(i, j - 1)
-            return max(take_left, take_right)
+        # whose turn it is
+        # turn is divisible by 2 player 1 other wise 2
+        # left and right poitners
+        # player 1 points and player 2 points
+        # make left and right indexes inclusive
 
-        return best_diff(0, len(nums) - 1) >= 0
+        def dfs(turn, left, right, player_1_score, player_2_score):
+            if right < left:
+                return player_1_score >= player_2_score
+            if turn % 2 == 0:
+                return dfs(turn+1, left+1,right, player_1_score + nums[left], player_2_score) or dfs(turn+1, left,right-1, player_1_score + nums[right], player_2_score)
+            else:
+                return dfs(turn+1, left+1,right, player_1_score, player_2_score + nums[left]) and dfs(turn+1, left,right-1, player_1_score, player_2_score+ nums[right])
+
+        return dfs(0, 0, len(nums)-1, 0, 0 )
