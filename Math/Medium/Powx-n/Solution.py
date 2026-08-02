b8e3b9d486af03286ea3d4e447ab8f996b6f9502
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-03 01:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (86.14%) - GetLeet

--- Math/Medium/Powx-n/Solution.py
+++ Math/Medium/Powx-n/Solution.py
@@ -1,19 +1,27 @@
-class Solution(object):
-    def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        def power(x, n):
-            if n == 0:
-                return 1.0
-            if n == 1:
-                return x
-            if n % 2 == 0:
-                return power(x * x, n // 2)
-            return x * power(x, n - 1)
+class Solution:
+    def power(self, x, n):
+        # Base case: anything raised to 0 is 1
+        if n == 0:
+            return 1.0
+        
+        # Base case: anything raised to 1 is itself
+        if n == 1:
+            return x
+        
+        # If 'n' is even
+        if n % 2 == 0:
+            # Recursive call: x * x, n // 2
+            return self.power(x * x, n // 2)
+        
+        # If 'n' is odd
+        # Recursive call: x * power(x, n - 1)
+        return x * self.power(x, n - 1)
 
+    def myPow(self, x, n):
+        # If 'n' is negative
         if n < 0:
-            return 1.0 / power(x, -n)
-        return power(x, n)
+            # Calculate the power of -n and take reciprocal
+            return 1.0 / self.power(x, -n)
+        
+        # If 'n' is non-negative
+        return self.power(x, n)
