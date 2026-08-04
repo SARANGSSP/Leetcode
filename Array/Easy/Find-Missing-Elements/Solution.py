54545e448ff837cbb10bf8baa21d842042ff7dea
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-04 12:52:34 +0530

    Time: 2 ms (75.11%) | Memory: 19.2 MB (55.74%) - GetLeet

--- Array/Easy/Find-Missing-Elements/Solution.py
+++ Array/Easy/Find-Missing-Elements/Solution.py
@@ -1,9 +1,5 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        a = max(nums)
-        b = min(nums)
-        result = []
-        for i in range(b,a+1):
-            if i not in nums:
-                result.append(i)
-        return result
+        num_set = set(nums)
+        low, high = min(nums), max(nums)
+        return [i for i in range(low, high + 1) if i not in num_set]
