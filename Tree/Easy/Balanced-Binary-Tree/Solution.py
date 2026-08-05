c9054ce37995fcc69b512a8e4a3508fc937fa407
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-06 03:03:44 +0530

    Time: 7 ms (17.54%) | Memory: 20.4 MB (70.15%) - GetLeet

---
+++ Tree/Easy/Balanced-Binary-Tree/Solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root == None:
+            return True
+        Flag = True
+        def height(root):
+            nonlocal Flag
+            if root == None:
+                return 0
+            lh = height(root.left)
+            rh = height(root.right)
+            if lh or rh:
+                if abs(lh-rh) > 1:
+                    Flag = False
+            return max(lh,rh) + 1
+        height(root)
+        return Flag
+        
