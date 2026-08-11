d7cf73d6e7f9aed5915808ec9806aa2af326b403
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-11 15:11:23 +0530

    Time: 37 ms (52.40%) | Memory: 31.6 MB (5.97%) - GetLeet

--- Array/Medium/Maximum-Subarray/Solution.py
+++ Array/Medium/Maximum-Subarray/Solution.py
@@ -1,8 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return nums[0]
         sumi = 0
         maxsum = float(-inf)
         for i in range(n):
