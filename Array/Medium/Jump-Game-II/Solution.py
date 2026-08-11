abbb8b22d296c3c4c073cbbc4de2971e766fc11e
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-11 19:23:14 +0530

    Time: 6 ms (55.51%) | Memory: 20.1 MB (65.32%) - GetLeet

--- Array/Medium/Jump-Game-II/Solution.py
+++ Array/Medium/Jump-Game-II/Solution.py
@@ -2,11 +2,11 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
         jumps = 0
-        currentEnd = 0
+        current_end = 0
         farthest = 0
         for i in range(n - 1):
             farthest = max(farthest, i + nums[i])
-            if i == currentEnd:
+            if i == current_end:
                 jumps += 1
-                currentEnd = farthest
+                current_end = farthest
         return jumps
