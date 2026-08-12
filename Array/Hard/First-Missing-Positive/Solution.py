f0fd0eefc4f1021a37673d8aa1076b1026edaf48
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-12 19:24:01 +0530

    Time: 49 ms (57.41%) | Memory: 30.9 MB (60.92%) - GetLeet

--- Array/Hard/First-Missing-Positive/Solution.py
+++ Array/Hard/First-Missing-Positive/Solution.py
@@ -1,11 +1,12 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n = max(nums)
-        hashmap = Counter(nums)
-        for i in range(1,n):
-            if i not in hashmap:
-                return i
-        if n > 0:
-            return n+1
-        else:
-            return 1
+        n = len(nums)
+        for i in range(n):
+            while 1 <= nums[i] <= n and  nums[nums[i] - 1] != nums[i]:
+                target = nums[i] - 1
+                nums[i], nums[target] = nums[target], nums[i]
+        
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        return n+1
