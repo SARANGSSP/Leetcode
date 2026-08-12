0fb7ca39be221eaf1ff201d8ca5da1b3d8187a63
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-12 21:47:39 +0530

    Time: 11 ms (31.42%) | Memory: 19.7 MB (38.45%) - GetLeet

---
+++ Two-Pointers/Easy/Valid-Palindrome/Solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        string = ""
+        for char in s:
+            if char.isalnum():
+                string += char.lower()
+        for i in range(len(string)//2):
+            if string[i] != string[len(string)-1-i]:
+                return False
+        return True
+
+
+        
