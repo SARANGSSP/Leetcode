9fc84bb3da24c7045fdaa5b3134f41995e5cab67
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-12 22:06:53 +0530

    Time: 3808 ms (9.88%) | Memory: 19.7 MB (22.18%) - GetLeet

---
+++ Hash-Table/Easy/First-Unique-Character-in-a-String/Solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for idx,char in enumerate(s):
+            if s.count(char) == 1:
+                return idx
+        return -1
