e9aa90c14a6689faf5e82333ff6c872c59b48f2f
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-08-16 23:53:09 +0530

    Time: 10 ms (9.03%) | Memory: 31.4 MB (48.27%) - GetLeet

---
+++ Array/Medium/Peak-Index-in-a-Mountain-Array/Solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        n = len(arr)
+        for i in range(n):
+            if arr[i] > arr[i+1] and arr[i] > arr[i-1]:
+                return i
+            
