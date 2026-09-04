e2c92ca408ad23c2814a947c871aa7fb317787b1
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-09-04 11:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (54.63%) - GetLeet

--- Array/Easy/Contains-Duplicate/Solution.py
+++ Array/Easy/Contains-Duplicate/Solution.py
@@ -1,8 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = hashmap.get(nums[i],0) + 1
-            if hashmap[nums[i]] == 2:
-                return True
-        return False
+        return(len(set(nums)) != len(nums))
