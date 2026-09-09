659e11456ec1da88d45d74ec78ba0ec47fe36b79
SARANG SUSHANT PANDIT <67600205+SARANGSSP@users.noreply.github.com>
2026-09-09 19:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (87.23%) - GetLeet

--- Array/Easy/Check-if-Array-Is-Sorted-and-Rotated/Solution.py
+++ Array/Easy/Check-if-Array-Is-Sorted-and-Rotated/Solution.py
@@ -2,13 +2,9 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         n = len(nums)
         diff = 0
-        idx = 0
-        for i in range(1,n):
-            if nums[i] < nums[i-1]:
-                idx = i
-        if idx == 0:
-            return True
-        else:
-            return ((nums[:idx] == sorted(nums[:idx])) and (nums[idx:] == sorted(nums[idx:])) 
-            and  nums[-1] <= nums[0])
+        for i in range(n):
+            if nums[i] > nums[(i+1)%n]:
+                diff += 1
+
+        return diff <= 1
 
